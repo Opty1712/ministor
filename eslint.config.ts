86325f6dbe08e93c0aa4f11145a8766a087e8eaa
Андрей Едунов <opty@opty.ru>
2026-06-01 14:41:56 +0300

    eslint fix

--- eslint.config.ts
+++ eslint.config.ts
@@ -30,7 +30,7 @@ export default defineConfig(
     languageOptions: { globals: globals.node },
   },
 
-  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+  globalIgnores(["**/.next/**", "**/out/**", "**/build/**", "**/next-env.d.ts"]),
   {
     settings: {
       react: { version: "19.2.4" },
